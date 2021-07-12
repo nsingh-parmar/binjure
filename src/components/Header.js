d64bbf66f881cf7e9d1539432ad1dfc52e3979a1
Nishant Parmar <nsingh-parmar@myseneca.ca>
2021-07-12 05:50:21 +0530

    New nav buttons
    Adding additional controls in the navbar; updating CSS rules for dynamic layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,17 @@ const Header = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark">
 			<Link to="/" className="navbar-brand">binjure</Link>
+
 			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 			</button>
 
 			<div className="collapse navbar-collapse" id="navbarContent">
+				<ul className="navbar-nav ml-auto" id="nav-buttons">
+					<li><Link to="/movies">Movies</Link></li>
+					<li><Link to="/shows">Series</Link></li>
+					<li><Link to="/genres">Genres</Link></li>
+				</ul>
 				<ul className="navbar-nav ml-auto" id="nav-buttons">
 					<li>
 						<Link to="/login">Sign In</Link>
